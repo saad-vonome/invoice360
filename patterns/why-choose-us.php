8c5bf1af5134828fbec17bfe6028f4088dee0d90
saad-vonome <saad@vonome.com>
2025-03-20 14:57:09 +0600

    Why Choose Us Section Created with responsiveness

---
+++ patterns/why-choose-us.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Title: Why Choose Us
+ * Slug: invoice-360-vonome/why-choose-us
+ * Categories: home
+ * Block Types: core/template-part/why-choose-us
+ * Description: Why Choose us section with our features.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<section class="why-choose-us">
+	<div class="container max-xl mx-auto py-24 md:py-36">
+		<div class="md:flex items-center justify-between mb-10 md:mb-14">
+			<h2>কেন Invoice360 আপনার জন্য সেরা পছন্দ?</h2>
+			<p class="why-choose-details">
+				আমাদের অত্যাধুনিক Invoice Management Software আপনার ব্যবসার লেনদেন সহজ, দ্রুত এবং নির্ভুলভাবে পরিচালনা করতে সাহায্য করবে।
+			</p>
+		</div>
+		<div class="grid grid-cols-1 md:grid-cols-3 gap:6 md:gap-10">
+			<!-- Easy Use -->
+			<div>
+				<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/easy-use.png" alt="সহজ ব্যবহার" />
+				<h5 class="pt-6">সহজ ব্যবহার</h5>
+				<p>ইউজার-ফ্রেন্ডলি ইন্টারফেস, ঝামেলামুক্ত অভিজ্ঞতা নিশ্চিত করে।</p>
+			</div>
+			<!-- Payment Tracking -->
+			<div>
+				<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/payment-tracking.png" alt="পেমেন্ট ট্র্যাকিং" />
+				<h5 class="pt-6">পেমেন্ট ট্র্যাকিং</h5>
+				<p>বকেয়া ও পরিশোধিত ইনভয়েস সহজেই পর্যবেক্ষণ করুন।</p>
+			</div>
+			<!-- Multiple Payment Option-->
+			<div>
+				<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/multi-payment-option.png" alt="মাল্টিপল পেমেন্ট অপশন" />
+				<h5 class="pt-6">মাল্টিপল পেমেন্ট অপশন</h5>
+				<p>ব্যাংক, মোবাইল ব্যাংকিং ও অনলাইন পেমেন্ট সাপোর্ট।</p>
+			</div>
+			<!-- Detailed Reporting -->
+			<div>
+				<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/detailed-reporting.png" alt="বিস্তারিত রিপোর্টিং" />
+				<h5 class="pt-6">বিস্তারিত রিপোর্টিং</h5>
+				<p>ব্যবসার রাজস্ব ও লেনদেনের বিশদ বিশ্লেষণ করুন।</p>
+			</div>
+			<!-- Multi Business Management -->
+			<div>
+				<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/multi-business.png" alt="মাল্টি-বিজনেস ম্যানেজমেন্ট " />
+				<h5 class="pt-6">মাল্টি-বিজনেস ম্যানেজমেন্ট </h5>
+				<p>একাধিক ব্যবসা একই প্ল্যাটফর্ম থেকে পরিচালনা করুন।</p>
+			</div>
+			<!-- 24/7 Support-->
+			<div>
+				<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/support.png" alt="২৪/৭ সাপোর্ট " />
+				<h5 class="pt-6">২৪/৭ সাপোর্ট </h5>
+				<p>যে কোনো সময় দ্রুত ও বিশ্বস্ত কাস্টমার সাপোর্ট পান।</p>
+			</div>
+		</div>
+	</div>
+</section>
