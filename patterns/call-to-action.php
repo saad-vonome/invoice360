996f48622b73955df86e93caff0cb16220e681c3
saad-vonome <saad@vonome.com>
2025-03-23 12:45:59 +0600

    Call to action section added

---
+++ patterns/call-to-action.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Title: Call to Action Section
+ * Slug: invoice-360-vonome/call-to-action
+ * Categories: home
+ * Block Types: core/template-part/call-to-action
+ * Description: Invoice 360 - Call to Action Section.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<section class="cta">
+    <div class="container max-xl mx-auto cta-content">
+        <img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/images/call-to-action.png" alt="একাউন্ট তৈরি করুন" class="hidden md:block" />
+        <div>
+            <h2>৭৫% সময় সাশ্রয় করুন – ইনভয়েস তৈরি করুন ৩০ সেকেন্ডের মধ্যে!</h2>
+            <a href="https://app.invoice360.xyz/signup" class="btnPrimary actionBtn block md:inline mx-auto">একাউন্ট তৈরি করুন</a>
+        </div>
+    </div>
+</section>
