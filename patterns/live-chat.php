8a913305e3b443d98668e05ea9408124dede2c63
saad-vonome <saad@vonome.com>
2025-03-26 13:05:34 +0600

    Live chat section completed

---
+++ patterns/live-chat.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Title: Invoice 360 - Live Chat section
+ * Slug: invoice-360-vonome/live-chat
+ * Categories: Common
+ * Block Types: core/template-part/live-chat
+ * Description: Invoice 360 - Live Chat section.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<section>
+	<div class="container max-xl flex items-center justify-between live-chat">
+		<div class="py-10 md:py-20 pl-4 md:pl-10">
+			<h1>লাইভ চ্যাটে – দ্রুত সমাধান পান!</h1>
+			<p>আমাদের সাপোর্ট এজেন্টরা সরাসরি লাইভ চ্যাটে আপনার সমস্যার সমাধান দিতে প্রস্তুত।</p>
+			<a class="btnPrimaryAlt flex items-center gap-2 max-w-fit bg-white" href="http://wa.me/88018XXXXXXXX">
+				<i class="fa-brands fa-whatsapp fa-xl"></i>
+				লাইভ চ্যাট করুন
+			</a>
+		</div>
+		<img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/images/live-chat-agent.png" alt="লাইভ চ্যাটে – দ্রুত সমাধান পান!" />
+	</div>
+</section>
